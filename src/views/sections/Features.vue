b4965464013b35bfca0f1421475e75b704670037
unknown <khanhnqps10920@fpt.edu.vn>
2020-11-13 19:15:49 +0700

    new code

--- src/views/sections/Features.vue
+++ src/views/sections/Features.vue
@@ -3,52 +3,62 @@
     <v-responsive class="mx-auto" max-width="1350">
       <v-container fluid>
         <v-row>
-          <v-col
-            v-for="card in [1, 2, 3, 4, 5, 6, 7]"
-            :key="card.title"
-            cols="12"
-            sm="4"
-            md="3"
-          >
-            <v-card elevation="2" height="400" class="primary"> </v-card>
+          <v-col v-for="item in data" :key="item.Id" cols="12" sm="4" md="3">
+            <v-card class="subject" elevation="2" max-height="500">
+              <v-img
+                height="300"
+                :src="require(`@/assets/${item.Logo}`)"
+              ></v-img>
+              <v-card-title class="">
+                <h5 class="text-body-2 d-block mx-auto">
+                  {{ item.Name }}
+                </h5>
+              </v-card-title>
+            </v-card>
           </v-col>
         </v-row>
+        <v-btn elevation="2" class="d-block mt-5 mx-auto">
+          Watch More
+        </v-btn>
       </v-container>
     </v-responsive>
   </base-section>
 </template>
 
 <script>
+import subject from "@/mixins/subject";
+
 export default {
+  mixins: [subject],
   name: "SectionFeatures",
 
-  data: () => ({
-    cards: [
-      {
-        icon: "mdi-keyboard-outline",
-        title: "Trendy Design",
-        text:
-          "Efficiently unleash media information without cross-media value. Quickly maximize value timely deliverables schemas.",
-      },
-      {
-        icon: "mdi-camera-outline",
-        title: "Photography",
-        text:
-          "Efficiently unleash media information without cross-media value. Quickly maximize value timely deliverables schemas.",
-      },
-      {
-        icon: "mdi-pencil-outline",
-        title: "Brand Making",
-        text:
-          "Efficiently unleash media information without cross-media value. Quickly maximize value timely deliverables schemas.",
-      },
-      {
-        icon: "mdi-puzzle-outline",
-        title: "24/7 Support",
-        text:
-          "Efficiently unleash media information without cross-media value. Quickly maximize value timely deliverables schemas.",
-      },
-    ],
-  }),
+  computed: {
+    data() {
+      return this.subject.splice(this.subject.length - 8, this.subject.length);
+    }
+  }
 };
 </script>
+
+<style lang="scss">
+.subject {
+  cursor: pointer;
+
+  &:before {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: black;
+    opacity: 0.5;
+    content: "";
+    top: 0;
+    left: 0;
+    z-index: 2;
+    transition: 0.4s ease;
+  }
+
+  &:hover:before {
+    opacity: 0;
+  }
+}
+</style>
