0be6f3b5ac4fb985040b4dfb91ea5e8aa7197ba8
unknown <khanhnqps10920@fpt.edu.vn>
2020-11-15 15:38:36 +0700

    new code

--- src/views/quizz/Quizz.vue
+++ src/views/quizz/Quizz.vue
@@ -1,65 +1,101 @@
 <template>
   <v-card>
-    <v-container v-if="quizs">
-      <v-tabs
-        v-model="tab"
-        background-color="primary"
-        next-icon="mdi-chevron-right"
-        prev-icon="mdi-chevron-left"
+    <v-container v-if="!finish">
+      <v-toolbar>
+        <v-toolbar-title class="text-h5 primary--text">
+          {{ subjectItem.Name }}
+        </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-title>
+          {{ total }}
+        </v-toolbar-title>
+      </v-toolbar>
+
+      <v-card
+        v-for="(quiz, i) in quizs"
+        :key="quiz.Id"
+        class="invisible"
+        :class="{ active: i + 1 === current }"
       >
-        <v-tabs-slider color="blue darken-4"></v-tabs-slider>
-        <v-tab v-for="(question, i) in quizs" :key="question.Id">
-          {{ i + 1 }}
-        </v-tab>
-      </v-tabs>
+        <v-card-title>
+          <p class="text-h6">{{ i + 1 + " " + quiz.Text }}</p>
+          <v-spacer></v-spacer>
+          <p class="text-subtitle">{{ quiz.Marks }}</p>
+        </v-card-title>
+        <v-card-text>
+          <v-radio-group v-model="currentAnswer">
+            <v-radio
+              :label="answer.Text"
+              v-for="answer in quiz.Answers"
+              :key="answer.Id"
+              :value="answer.Id"
+            >
+            </v-radio>
+          </v-radio-group>
+        </v-card-text>
 
-      <v-tabs-items v-model="tab">
-        <v-tab-item v-for="question in quizs" :key="question.Id">
-          <v-card>
-            <v-container>
-              <h3 class="my-3">{{ question.Text }}</h3>
+        <v-card-actions>
+          <v-btn large @click="handleNext(quiz)">Next</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-container>
 
-              <v-radio-group>
-                <v-radio
-                  v-for="answer in question.Answers"
-                  :key="answer.Id"
-                  :label="answer.Text"
-                  :value="answer.Id"
-                >
-                </v-radio>
-              </v-radio-group>
-            </v-container>
-          </v-card>
-        </v-tab-item>
-      </v-tabs-items>
+    <v-container v-else>
+      <h1>finish</h1>
     </v-container>
   </v-card>
 </template>
 
 <script>
 import axios from "axios";
+import subject from "@/mixins/subject";
 
 export default {
+  mixins: [subject],
+
   data() {
     return {
-      tab: null,
+      currentAnswer: null,
+      current: 1,
+      total: 0,
       quizs: [],
-      step: 1,
-      score: 0,
-      stepAnswer: null,
-      done: false,
+      finish: false
     };
   },
 
-  created() {
-    console.log(this.$route.params.id);
-    // const data = axios.get(
-    //   `/${require(`@/data/Quizs/${this.$route.params.id}`)}`
-    // );
-    this.quizs = require(`@/data/Quizs/${this.$route.params.id}`);
+  methods: {
+    handleNext(quiz) {
+      if (this.current === this.quizs.length) this.finish = true;
+
+      if (!this.currentAnswer) {
+        alert("Hãy chọn 1 đáp án");
+        return;
+      }
+      // total
+      this.total =
+        quiz.AnswerId === this.currentAnswer
+          ? this.total + quiz.Marks
+          : this.total;
+
+      this.current += 1;
+    }
   },
+
+  created() {
+    this.subjectItem = this.subject.find(el => el.Id === this.$route.params.id);
+
+    this.quizs = require(`@/data/Quizs/${this.$route.params.id}`);
+  }
 };
 </script>
 
 <style>
+.invisible {
+  display: none;
+}
+.invisible.active {
+  display: block;
+}
 </style>
