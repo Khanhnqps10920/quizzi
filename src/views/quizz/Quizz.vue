7f8855d2bc4b9787ede304d4f49f8282da681d31
khanh <khanhnqps10920@fpt.edu.vn>
2020-11-16 12:14:06 +0700

    new code

--- src/views/quizz/Quizz.vue
+++ src/views/quizz/Quizz.vue
@@ -22,7 +22,7 @@
         <v-card-title>
           <p class="text-h6">{{ i + 1 + " " + quiz.Text }}</p>
           <v-spacer></v-spacer>
-          <p class="text-subtitle">{{ quiz.Marks }}</p>
+          <p class="text-subtitle">{{ quiz.Marks }} point</p>
         </v-card-title>
         <v-card-text>
           <v-radio-group v-model="currentAnswer">
@@ -38,12 +38,20 @@
 
         <v-card-actions>
           <v-btn large @click="handleNext(quiz)">Next</v-btn>
+
+          <v-btn color="primary" large @click="handleFinish">Finish</v-btn>
         </v-card-actions>
       </v-card>
     </v-container>
 
     <v-container v-else>
-      <h1>finish</h1>
+      <v-layout>
+        <v-flex sm12>
+          <p>Your score is {{ total }} / {{ totalScore }}</p>
+
+          <v-btn to="/">Go Home</v-btn>
+        </v-flex>
+      </v-layout>
     </v-container>
   </v-card>
 </template>
@@ -61,7 +69,8 @@ export default {
       current: 1,
       total: 0,
       quizs: [],
-      finish: false
+      finish: false,
+      totalScore: 0,
     };
   },
 
@@ -80,14 +89,25 @@ export default {
           : this.total;
 
       this.current += 1;
-    }
+      this.currentAnswer = null;
+    },
+
+    handleFinish() {
+      this.finish = true;
+    },
   },
 
   created() {
-    this.subjectItem = this.subject.find(el => el.Id === this.$route.params.id);
+    this.subjectItem = this.subject.find(
+      (el) => el.Id === this.$route.params.id
+    );
 
     this.quizs = require(`@/data/Quizs/${this.$route.params.id}`);
-  }
+
+    this.totalScore = this.quizs.reduce((acc, cur) => {
+      return acc + cur.Marks;
+    }, 0);
+  },
 };
 </script>
 
